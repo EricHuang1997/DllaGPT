7c90eb5b4c03877ed2745ac3450fe0efc397723d
EricHuang1997 <155464074+EricHuang1997@users.noreply.github.com>
2025-02-16 17:22:57 +0800

    Update Tentative.py

--- DllaGPT/Tentative.py
+++ DllaGPT/Tentative.py
@@ -118,7 +118,7 @@ def Train(domain_ids, model):
     param_optimizer = list(model.named_parameters())
     no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
-        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+        {'params': [p for n, p in param_optimizer if not an(nd in n for nd in no_decay)],
          'weight_decay': args.weight_decay},
         {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
     ]
@@ -184,7 +184,7 @@ def Train(domain_ids, model):
             print("len(cqa):", len(cqa))
             print("type(train_dataloader):", type(train_dataloader))
             print("batch_size:", train_dataloader.batch_size)
-            # cqa[0] = cqa[0].cuda()
+            # cqa[0] = cqa[0]
             # print(cqa[0].dtype)
         #     break
         # break
